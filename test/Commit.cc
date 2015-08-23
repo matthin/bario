9600c9632ff530d7fd55802785cf5ae43a5fe151
Justin Harrison <justin@matthin.com>
2015-08-23 19:40:21 -0400

    Reduce repeated code

--- test/Commit.cc
+++ test/Commit.cc
@@ -4,31 +4,28 @@
 #include <string>
 #include <vector>
 
-TEST_CASE("Commit stores required data", "[Commit]") {
+TEST_CASE("General Commit tests", "[Commit]") {
   const std::vector<ba::Contrast> contrasts = {
     ba::Contrast("index.html", "Hello")
   };
   const std::string email("example@test.com");
   const std::string message("Example message");
-  const ba::Commit commit(contrasts, email, message);
-  REQUIRE(commit.contrasts == contrasts);
-  REQUIRE(commit.email == email);
-  REQUIRE(commit.message == message);
-}
-
-TEST_CASE("Commit creates ID") {
-  const ba::Commit commit({
-    ba::Contrast("index.html", "Hello")
-  }, "example@test.com", "Example message");
-  REQUIRE(commit.id.length() > 0);
-}
-
-TEST_CASE("Commit creates date") {
   const auto startDate = std::time(nullptr);
-  const ba::Commit commit({
-    ba::Contrast("index.html", "Hello")
-  }, "example@test.com", "Example message");
-  const auto endDate = std::time(nullptr);
-  REQUIRE(startDate <= commit.date <= endDate);
+  const ba::Commit commit(contrasts, email, message);
+
+  SECTION("Commit stores required data", "[Commit]") {
+    REQUIRE(commit.contrasts == contrasts);
+    REQUIRE(commit.email == email);
+    REQUIRE(commit.message == message);
+  }
+
+  SECTION("Commit creates ID") {
+    REQUIRE(commit.id.length() > 0);
+  }
+
+  SECTION("Commit creates date") {
+    const auto endDate = std::time(nullptr);
+    REQUIRE(startDate <= commit.date <= endDate);
+  }
 }
 
